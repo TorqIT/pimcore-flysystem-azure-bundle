01ba03b03aa1af0283c0794173c79ddab8db2c65
Evan Jackson <evan@jacksonnet.ca>
2023-10-10 16:50:06 -0300

    Configured correctly for Pimcore 11

--- src/DependencyInjection/FlysystemAzureExtension.php
+++ src/DependencyInjection/FlysystemAzureExtension.php
@@ -19,7 +19,10 @@ class FlysystemAzureExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('config.yaml');
+        $loader->load('services.yaml');
     }
 }
